9490d17556f20a66713cfc30bfbd86848a4100ff
Asfia Numani <asfi_numani@hotmail.com>
2014-12-11 15:53:45 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,39 @@
 // Super Class to represent a person
+public class Person
+{
+  private String firstname;
+  private String lastname;
+  private int age;
+  public Person()
+  {
+    firstname="";
+    lastname="";
+    age=0;
+  }
+  public Person(String one, String two, int x)
+  {
+    firstname=one;
+    lastname=two;
+    age=x;
+  }
+  public String getFirstName()
+  {
+    return firstname;
+  }
+  public String getLastName()
+  {
+    return lastname;
+  }
+  public int getAge()
+  {
+    return age;
+  }
+  public String toString()
+  {
+    return lastname+", "+firstname;
+  }
+  public void recordBirthday()
+  {
+    age++;
+  }
+}
