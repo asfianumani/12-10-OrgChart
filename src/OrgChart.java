91482e4db9de1f3572368888631e1d12bdd5cdcd
Asfia Numani <asfi_numani@hotmail.com>
2014-12-16 08:19:20 -0700

    Update OrgChart.java

--- src/OrgChart.java
+++ src/OrgChart.java
@@ -16,11 +16,7 @@ public class OrgChart
     Manager second=new Manager("Heavensbee", "Plutarch", 34, 44.7, 7, "Developement Manager", "Developement");
 
     Employee pt2=new Employee("Finnick", "Odair", 24, 28.5, 7, "JR Developer");
-
-                                
-
-                                
-
+     
     System.out.println(first);
 
     System.out.println("\t"+pt);
@@ -30,7 +26,39 @@ public class OrgChart
     System.out.println(second);
 
     System.out.println("\t"+pt2);  
+    
+    printInfo(first);
+    printInfo(pt);
+    printInfo(lt);
+    printInfo(second);
+    printInfo(pt2);
+
+
+  }                           
+
+    public static void printInfo(Person p)
+    {
+      if(p instanceof Manager)
+      {
+        Manager m=(Manager) p;
+        System.out.println("Department:"+m.getDepartment());
+        System.out.println();
+      }
+       if(p instanceof Employee)
+       {
+        Employee e= (Employee) p;
+        System.out.println("Wage:"+e.getWage());
+        System.out.println("Hours:"+e.getHours());
+        System.out.println("Job Title:"+e.getJobTitle());
+        System.out.println();
+       }
+       
+        System.out.println("First Name:"+p.getFirstName());
+        System.out.println("Last Name:"+p.getLastName());
+        System.out.println("Age:"+p.getAge());
+        System.out.println();
 
   }                             
 
 }
+
